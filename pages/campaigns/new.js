47a306ef7cdb79c4d375cc7cac903d8ed46d882a
Max Proano <proanom89@gmail.com>
2021-04-28 15:46:44 -0700

    Display loading to user

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -7,11 +7,14 @@ import web3 from '../../ethereum/web3.js';
 class CampaignNew extends Component {
   state = {
     minimumContribution: '',
-    errorMessage: ''
+    errorMessage: '',
+    loading: false
   }
 
   onSubmit = async (event) => {
     event.preventDefault();
+
+    this.setState({ loading: true, errorMessage: '' });
 
     try {
       const accounts = await web3.eth.getAccounts();
@@ -22,6 +25,8 @@ class CampaignNew extends Component {
     } catch (err) {
       this.setState({ errorMessage: err.message});
     }
+
+    this.setState({ loading: false });
   };
 
   render() {
@@ -40,7 +45,7 @@ class CampaignNew extends Component {
           </Form.Field>
 
           <Message error header="Whoops!" content={this.state.errorMessage} />
-          <Button primary>Create</Button>
+          <Button loading={this.state.loading} primary>Create</Button>
         </Form>
       </Layout>
     )
